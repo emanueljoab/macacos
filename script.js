dac203e4cd7be7302c22dd490c42c1082946bed1
Emanuel Joab <60127293+emanueljoab@users.noreply.github.com>
2022-08-02 23:26:24 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let macaco = ['Orangotango', 'Chimpanzé', 'Gorila', 'Babuíno', 'Macaco-aranha', 'Macaco-prego']
+console.log(macaco)
+function gerar(){
+    let aleatorio = macaco[Math.floor(Math.random() *macaco.length)];
+    res.innerHTML = `<strong>${aleatorio}</strong>`
+    res.style.textAlign = 'center'
+    var img = document.createElement('img')
+    foto.innerHTML= ''
+    img.setAttribute('width', '500px')
+    img.setAttribute('height', '600px')
+    if (aleatorio == ['Orangotango']) {
+        img.setAttribute('src', 'foto-orangotango.jpg')
+    } else if (aleatorio == ['Chimpanzé']) {
+        img.setAttribute('src', 'foto-chimpanze.jpg')
+    } else if (aleatorio == ['Gorila']) {
+        img.setAttribute('src', 'foto-gorila.jpg')
+    } else if (aleatorio == ['Babuíno']) {
+        img.setAttribute('src', 'foto-babuino.jpg')
+    } else if (aleatorio == ['Macaco-aranha']) {
+        img.setAttribute('src', 'foto-macacoaranha.jpg')
+    } else {
+        img.setAttribute('src', 'foto-macacoprego.jpg')
+    }
+        foto.appendChild(img)
+}
